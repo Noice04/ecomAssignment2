2b96a9c005901566cd6778b466b374f93f47d389
Noice04 <113056083+Noice04@users.noreply.github.com>
2025-04-26 17:50:44 -0400

    Delete app directory

--- app/Resources/Views/templates/footer.php
+++
@@ -1,6 +0,0 @@
-
-
-<h1>This is a footer</h1>
-
-</body>
-</html>
--- app/regex.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-
-echo preg_match('/H.T/', "HaT"); // ->TRUE
-echo "</br>";
-echo preg_match('/H.T/', "HaaT");// -> False because the dot '.' designate only one single character
-echo "</br>";
-echo preg_match('/^e+/', "pak");
